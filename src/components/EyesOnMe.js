a07be98e971c6fe5c8722f1e6db217d8a16fbf38
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-04-17 16:33:22 +0300

    event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe(){
+    const HandleFocus=function(){
+        console.log('Good!')
+    }
+    const HandleBlur=function(){
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <>
+        <button onFocus={HandleFocus} onBlur={HandleBlur}>
+            Eyes on me
+        </button>
+        </>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleChange=function(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                placeholder="Enter password"
+                onChange={handleChange}
+                className="p-2 border rounded"
+            />
+        </div>
     )
 }
 
